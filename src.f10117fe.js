e52af09449546c56553b5806ff8b375cc96f2b96
Artem Tyurin <artem.tyurin@uber.com>
2019-04-10 00:03:44 -0700

    Updates

--- src.f10117fe.js
+++ src.f10117fe.js
@@ -184,7 +184,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60052" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63026" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
