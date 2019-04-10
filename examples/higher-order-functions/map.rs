a09be3ab7071e2e3984e6b5aa51233bc1b64cbce
Artem Tyurin <artem.tyurin@uber.com>
2019-04-10 12:03:48 -0700

    Update Rust example

--- examples/higher-order-functions/map.rs
+++ examples/higher-order-functions/map.rs
@@ -1,15 +1,15 @@
 fn map<T, U>(
   arr: Vec<T>,
-  f: &Fn(&T) -> U,
+  mapper: impl Fn(&T) -> U,
 ) -> Vec<U> {
   let mut result = Vec::new();
   for item in arr.iter() {
-    result.push(f(item))
+    result.push(mapper(item))
   }
   return result;
 }
 
 fn main() {
-  let new_vec = map(vec![1, 2, 3], &|n| n * 2);
+  let new_vec = map(vec![1, 2, 3], |n| n * 2);
   print!("{:?}", new_vec);
 }
